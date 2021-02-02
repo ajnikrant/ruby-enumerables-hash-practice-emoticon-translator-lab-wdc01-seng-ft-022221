9d5c7cfb2650db170512e823835bda6b46569d2d
Amanda Nikrant <ajnikrant@gmail.com>
2021-02-02 21:35:47 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,29 +3,29 @@ require 'yaml'
 require 'pry'
 
 
-# def load_library(emoji_file)
-#   new_file = YAML.load_file(emoji_file)
-#
-#   final_result= new_file.each_with_object ({}) do |(descriptor, emoji), new_hash|
-#     new_hash[descriptor]= {"english": emoji[0], "japanese": emoji[1]}
-#   end
-#   # binding.pry
-# end
+def load_library(emoji_file)
+  new_file = YAML.load_file(emoji_file)
 
-def load_library(path)
-  emoji = YAML.load_file(path)
-
-
-  emoji_hash = {}
-  emoji_hash["get_meaning"] = {}
-  emoji_hash["get_emoticon"] = {}
-
-  emoji.each do |word, emojis|
-    emoji_hash["get_meaning"][emojis[1]] = word
-    emoji_hash["get_emoticon"][emojis[0]] = emojis[1]
+  final_result= new_file.each_with_object ({}) do |(descriptor, emoji), new_hash|
+    new_hash[descriptor]= {"english": emoji[0], "japanese": emoji[1]}
   end
-  emoji_hash
+  # binding.pry
 end
+
+# def load_library(path)
+#   emoji = YAML.load_file(path)
+# 
+# 
+#   emoji_hash = {}
+#   emoji_hash["get_meaning"] = {}
+#   emoji_hash["get_emoticon"] = {}
+# 
+#   emoji.each do |word, emojis|
+#     emoji_hash["get_meaning"][emojis[1]] = word
+#     emoji_hash["get_emoticon"][emojis[0]] = emojis[1]
+#   end
+#   emoji_hash
+# end
 
 
 
