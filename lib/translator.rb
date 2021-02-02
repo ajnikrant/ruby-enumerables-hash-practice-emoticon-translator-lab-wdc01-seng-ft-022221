2728e1af1f638d8994a0a55816fa46b348858460
Amanda Nikrant <ajnikrant@gmail.com>
2021-02-02 21:35:50 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -14,12 +14,12 @@ end
 
 # def load_library(path)
 #   emoji = YAML.load_file(path)
-# 
-# 
+#
+#
 #   emoji_hash = {}
 #   emoji_hash["get_meaning"] = {}
 #   emoji_hash["get_emoticon"] = {}
-# 
+#
 #   emoji.each do |word, emojis|
 #     emoji_hash["get_meaning"][emojis[1]] = word
 #     emoji_hash["get_emoticon"][emojis[0]] = emojis[1]
